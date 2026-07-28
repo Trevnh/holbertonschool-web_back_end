f1dd3a9a8e84a457d609da3ae0ceae32a10717f8
Trevnh <trevorhuynh@hotmail.com>
2026-07-29 09:59:23 +1000

    Add 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for annotated concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Annotated concat function to concatenate str1 and str2"""
+    return str1 + str2
