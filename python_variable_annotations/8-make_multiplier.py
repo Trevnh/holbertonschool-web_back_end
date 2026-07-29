ba7059adcf6a2d05f58f2a3042bc8d1c39a7dfe8
Trevnh <trevorhuynh@hotmail.com>
2026-07-29 11:36:35 +1000

    Fix logic real

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,7 +6,4 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Function that takes a float and returns a function to multiply"""
-    def multiply(multiplier: float) -> float:
-        """Function to multiply a number by itself"""
-        return multiplier * multiplier
-    return multiply
+    return lambda num: num * multiplier
