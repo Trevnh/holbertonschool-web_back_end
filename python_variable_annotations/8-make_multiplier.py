19335eb1effb6734536e76c712df5060c8e4ca99
Trevnh <trevorhuynh@hotmail.com>
2026-07-29 11:25:43 +1000

    Fix logic

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Function that takes a float and multiplies it by itself"""
-    return multiplier * multiplier
-    
+    """Function that takes a float and returns a function to multiply"""
+    def multiply(multiplier: float) -> float:
+        """Function to multiply a number by itself"""
+        return multiplier * multiplier
+    return multiply
