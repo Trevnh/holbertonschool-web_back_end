000707196cbe474cc6728ef36dc41b88a9729334
Trevnh <trevorhuynh@hotmail.com>
2026-07-29 10:07:33 +1000

    Add 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for annotated to_str function"""
+
+
+def to_str(n: float) -> str:
+    """Annotated to_str function to return n (float) as a str"""
+    return str(n)
