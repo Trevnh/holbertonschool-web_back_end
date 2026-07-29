0028665082712a42d4652b18511768d2e7501654
Trevnh <trevorhuynh@hotmail.com>
2026-07-29 10:41:27 +1000

    Add 6-sum_mixed_list.py

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for annotated sum_mixed_list function"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Function to sum a list of floats"""
+    return sum(mxd_lst)
