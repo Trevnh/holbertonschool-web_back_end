48f7a07e7177a046a8dbbe0a29804bea51eecab1
Trevnh <trevorhuynh@hotmail.com>
2026-08-02 09:39:04 +0200

    Add 4-tasks.py and make previous files executable

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module for multiple tasks"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times with given max_delay
+    and return the delays in a list
+    """
+    tasks = [task_wait_random(max_delay)
+             for _ in range(n)]
+
+    completed: List[float] = []
+
+    for task in asyncio.as_completed(tasks):
+        completed.append(await task)
+
+    return completed
