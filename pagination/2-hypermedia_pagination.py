98cf15b8791ce32f890583f8294414a61985d0ae
Trevnh <trevorhuynh@hotmail.com>
2026-08-16 06:32:54 +0000

    Add 2-hypermedia_pagination.py

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Module for Hypermedia Pagination"""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """Function to return index range Tuple from page and page_size"""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Gets the paginated version of the data with the given
+        aspects of page and page_size
+        """
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+        (start, end) = index_range(page, page_size)
+
+        return self.dataset()[start:end]
+
+    def get_hyper(
+        self, page: int = 1, page_size: int = 10) -> Dict[
+            str, Union[int, List, None]]:
+    """Return page data alongside hypermedia data"""
+    data = self.get_page(page, page_size)
+
+    total_pages = math.ceil(
+        len(self.dataset() / page_size)) if self.dataset() else 0
+
+    next_page = page + 1 if page < total_pages else None
+    prev_page = page - 1 if page > 1 else None
+
+    return {
+        "page_size": page_size,
+        "page": page,
+        "data": data,
+        "next_page": next_page,
+        "prev_page": prev_page,
+        "total_pages": total_pages
+    }
