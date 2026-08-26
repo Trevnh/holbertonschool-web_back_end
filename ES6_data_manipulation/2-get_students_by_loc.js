5d739d96738fd60464415796944cf7602049046d
Trevnh <trevorhuynh@hotmail.com>
2026-08-26 10:57:20 +1000

    Add 2-get_students_by_loc.js

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,4 @@
+export default function getStudentsByLocation(students, location) {
+  const filteredStudents = students.filter(student => student.location === location);
+  return filteredStudents
+}
