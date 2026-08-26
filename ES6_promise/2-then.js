bf69639e33005bebd215745cc53327eec5b330eb
Trevnh <trevorhuynh@hotmail.com>
2026-08-26 15:40:39 +1000

    fix maybe

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   const promise1 = new Promise((resolve, reject) => {
-    resolve({'status': 200, 'body': 'Success'});
-    reject(new Error);
+    resolve({'status': 200, 'body': 'success'});
+    reject(new Error());
   });
   promise1.then(
     console.log('Got a response from the API')
