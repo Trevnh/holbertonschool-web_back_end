7342a2a62e5f424d0e6893c81c53489ddc377384
Trevnh <trevorhuynh@hotmail.com>
2026-08-30 17:39:39 +1000

    change logic

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,7 +3,17 @@ import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-  .then((results) => {
-    return (results);
-  });
+  .then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+        return {
+            status: result.status,
+            value: result.value,
+        };
+    }
+
+    return {
+        status: result.status,
+        value: result.reason.toString(),
+    }
+  }));
 }
