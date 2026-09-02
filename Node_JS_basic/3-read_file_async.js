7daa74503d5084b91971d89914890348740aad78
Trevnh <trevorhuynh@hotmail.com>
2026-09-02 12:58:38 +1000

    Add 3-read_file_async.js

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const data1 = data
+          .split('\n')
+          .map((e) => e.trim())
+          .map((e) => e.split(',').map((e) => e.trim()))
+          .filter((e) => e.length !== 1);
+        data1.shift();
+        console.log(`Number of students: ${data1.length}`);
+        const field = data1.map((row) => row[3]);
+        const nameField = data1.map((row) => [row[0], row[3]]);
+        const uniqueFields = [...new Set(field)];
+        uniqueFields.forEach((element) => {
+          const list = [];
+          nameField.forEach((e) => {
+            if (e[1] === element) {
+              list.push(e[0]);
+            }
+          });
+          console.log(`Number of students in ${element}: ${list.length}. List: ${list.join(', ')}`);
+        });
+        resolve(data1);
+      }
+    });
+  });
+  return promise;
+}
+
+module.exports = countStudents;
