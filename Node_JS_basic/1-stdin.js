61992550084b99d5618293bc80bfc15e5915fb4f
Trevnh <trevorhuynh@hotmail.com>
2026-09-02 10:38:03 +1000

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', data => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
   console.log('This important software is now closing');
-  process.exit();
 });
