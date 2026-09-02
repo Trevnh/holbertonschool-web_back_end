3f0dbb95b5365578b955a36f151273ec5f1f07c2
Trevnh <trevorhuynh@hotmail.com>
2026-09-02 10:32:01 +1000

    Add 1-stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', data => {
+  process.stdout.write(`Your name is: ${data}`);
+  console.log('This important software is now closing');
+  process.exit();
+});
