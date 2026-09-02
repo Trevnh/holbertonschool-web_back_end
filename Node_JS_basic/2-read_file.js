bcb280948daace04974e4e7a0ca732c1bfd30ec0
Trevnh <trevorhuynh@hotmail.com>
2026-09-02 12:27:38 +1000

    eslint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,28 +3,28 @@ const fs = require('node:fs');
 function countStudents(path) {
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
-        throw new Error('Cannot load the database');
+      throw new Error('Cannot load the database');
     }
-    data = data
-    .split('\n')
-    .map(e => e.trim())
-    .map(e => e.split(',').map(e => e.trim()))
-    .filter(e => e.length !== 1);
-    data.shift();
-    console.log(`Number of Students: ${data.length}`);
-    const field = data.map(row => row[3]);
-    const nameField = data.map(row => [row[0], row[3]]);
+    const data1 = data
+      .split('\n')
+      .map((e) => e.trim())
+      .map((e) => e.split(',').map((e) => e.trim()))
+      .filter((e) => e.length !== 1);
+    data1.shift();
+    console.log(`Number of Students: ${data1.length}`);
+    const field = data1.map((row) => row[3]);
+    const nameField = data1.map((row) => [row[0], row[3]]);
     const uniqueFields = [...new Set(field)];
-    uniqueFields.forEach(element => {
-        const list = [];
-        nameField.forEach(e => {
-            if (e[1] === element) {
-                list.push(e[0]);
-            }
-        })
-        console.log(`Number of Students in ${element}: ${list.length}. List: ${list.join(', ')}`);
+    uniqueFields.forEach((element) => {
+      const list = [];
+      nameField.forEach((e) => {
+        if (e[1] === element) {
+          list.push(e[0]);
+        }
+      });
+      console.log(`Number of Students in ${element}: ${list.length}. List: ${list.join(', ')}`);
     });
-  })
+  });
 }
 
 module.exports = countStudents;
