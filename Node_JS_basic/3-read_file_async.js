084598bd59f84663f0f9985da4dc94584ef82dca
Trevnh <trevorhuynh@hotmail.com>
2026-09-05 16:53:58 +1000

    Go back to this version for checker

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -6,33 +6,26 @@ function countStudents(path) {
       if (err) {
         reject(new Error('Cannot load the database'));
       } else {
-        // parse csv into array of arrays
         const data1 = data
           .split('\n')
           .map((e) => e.trim())
           .map((e) => e.split(',').map((e) => e.trim()))
           .filter((e) => e.length !== 1);
-        // turn array of arrays into array of objects
-        const [headers, ...rows] = data1;
-        const objArray = rows.map((row) => row.reduce((obj, val, index) => {
-          const updatedObj = { ...obj };
-          updatedObj[headers[index]] = val;
-          return updatedObj;
-        }, {}));
-        console.log(`Number of students: ${objArray.length}`);
-        // make an object with key value pair of {field: [firstnames]}
-        const fields = {};
-        objArray.forEach((e) => {
-          if (Object.hasOwn(fields, e.field)) {
-            fields[e.field].push(e.firstname);
-          } else {
-            fields[e.field] = [e.firstname];
-          }
+        data1.shift();
+        console.log(`Number of students: ${data1.length}`);
+        const field = data1.map((row) => row[3]);
+        const nameField = data1.map((row) => [row[0], row[3]]);
+        const uniqueFields = [...new Set(field)];
+        uniqueFields.forEach((element) => {
+          const list = [];
+          nameField.forEach((e) => {
+            if (e[1] === element) {
+              list.push(e[0]);
+            }
+          });
+          console.log(`Number of students in ${element}: ${list.length}. List: ${list.join(', ')}`);
         });
-        for (const [key, value] of Object.entries(fields)) {
-          console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
-        }
-        resolve(objArray);
+        resolve(data1);
       }
     });
   });
